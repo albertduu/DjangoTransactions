cd9e38a470e1282d388afd91686ce77b295a99fb
albertduu <alberthdu@gmail.com>
2025-06-25 19:30:04 -0400

    Fixed Add New Transaction

--- transactions/views.py
+++ transactions/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transaction
+from .forms import TransactionForm
 
 def transaction_list(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction-list')  # avoid resubmission on refresh
+    else:
+        form = TransactionForm()
+    
     transactions = Transaction.objects.all()
-    return render(request, 'transactions/list.html', {'transactions': transactions})
+    return render(request, 'transactions/list.html', {
+        'transactions': transactions,
+        'form': form
+    })
