1dcd560cdca8a9a5004af8f57f93cd725f875d99
albertduu <alberthdu@gmail.com>
2025-06-25 19:13:31 -0400

    Fixed Add New Transaction

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.transaction_list, name='transaction_list'),
     path('', views.transaction_list_create, name='transaction-list'),
 ]
