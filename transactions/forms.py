c85d3415c5cbae1e9b1fff9372e5490c0f061ba0
albertduu <alberthdu@gmail.com>
2025-06-25 19:35:41 -0400

    Fixed Add New Transaction

--- transactions/forms.py
+++ transactions/forms.py
@@ -1,7 +1,13 @@
 from django import forms
+from django.utils import timezone
 from .models import Transaction
 
 class TransactionForm(forms.ModelForm):
+    ts = forms.DateTimeField(
+        initial=timezone.now,
+        widget=forms.DateTimeInput(format='%Y-%m-%d %H:%M'),
+        input_formats=['%Y-%m-%d %H:%M', '%Y-%m-%d %H:%M:%S']
+    )
     class Meta:
         model = Transaction
-        fields = '__all__'  # or list specific fields like ['person_id', 'email', 'product']
+        fields = '__all__'
