512b2515a3fe510ca0308ec727e27a860661d4a9
albertduu <alberthdu@gmail.com>
2025-07-29 19:46:32 -0400

    Added files to email and fixed pagination

--- transactions/templatetags/querystring.py
+++ transactions/templatetags/querystring.py
@@ -4,15 +4,8 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def querystring(context, **kwargs):
-    """
-    Update query string params while preserving others.
-    Usage: {% querystring page=2 %}
-    """
     request = context['request']
     query = request.GET.copy()
-    for k, v in kwargs.items():
-        if v is None:
-            query.pop(k, None)
-        else:
-            query[k] = v
+    for key, value in kwargs.items():
+        query[key] = value
     return query.urlencode()
